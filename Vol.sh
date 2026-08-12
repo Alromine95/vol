4adac289c69de14cd155878d9c25b19343da5640
Alromine95 <abhinavsingh.me5@gmail.com>
2026-08-12 16:43:20 +0530

    Update Vol.sh

--- Vol.sh
+++ Vol.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf .repo/local_manifests
 
- repo init -u https://github.com/VoltageOS/manifest.git -b 16.2 --git-lfs
+ repo init -u https://github.com/VoltageOS/manifest.git --depth=1 -b 16.2 --git-lfs
 git clone https://github.com/Alromine95/android_local_manifests_blossom.git -b main .repo/local_manifests
 /opt/crave/resync.sh || repo sync
 
