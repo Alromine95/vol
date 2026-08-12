018e9cd6bc6f74d63872071dd6c1f072e5cae8d7
Alromine95 <abhinavsingh.me5@gmail.com>
2026-08-12 16:39:58 +0530

    Add files via upload

---
+++ Vol.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+rm -rf .repo/local_manifests
+
+ repo init -u https://github.com/VoltageOS/manifest.git -b 16.2 --git-lfs
+git clone https://github.com/Alromine95/android_local_manifests_blossom.git -b main .repo/local_manifests
+/opt/crave/resync.sh || repo sync
+
+export BUILD_USERNAME=Qbhi
+export BUILD_HOSTNAME=Pookie
+
+. build/envsetup.sh
+# run
+
+make installclean
+brunch blossom
+
+echo "Upload to GoFile will be started..."
+
+ZIP=$(find out/target/product/blossom -maxdepth 1 -type f -name "*.zip" | head -n 1)
+
+if [ -n "$ZIP" ]; then
+    echo "Uploading $ZIP..."
+    wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
+    chmod +x upload.sh
+    ./upload.sh "$ZIP"
+else
+    echo "No ROM ZIP found!"
+    exit 1
+fi
+
+
+
+
+
+
+
