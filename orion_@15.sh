8e26cf42ca41e5c0d5e9236bd66a7050b468a538
Alromine95 <abhinavsingh.me5@gmail.com>
2026-08-27 17:37:01 +0530

    Install necessary packages for build process
    Added package installation commands to the script.

--- orion_@15.sh
+++ orion_@15.sh
@@ -20,6 +20,13 @@ echo "============="
 echo "Sync success"
 echo "============="
 
+# Installing packages 
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses6 libncurses-dev libsdl1.2-dev libssl-dev libwxgtk3.2-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev;
+sudo apt-get update && sudo apt-get install patchelf coreutils -y
+echo "============="
+echo "packages done"
+echo "============="
+
 # Export
 export BUILD_USERNAME=Qbhi
 export BUILD_HOSTNAME=crave
