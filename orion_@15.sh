c0f1e2843f64382a1bb4e99796317487e1f22b87
Alromine95 <abhinavsingh.me5@gmail.com>
2026-08-27 17:29:13 +0530

    Update orion_@15.sh

--- orion_@15.sh
+++ orion_@15.sh
@@ -1,1 +1,37 @@
+#!/bin/bash
 
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/OrionOS-Project/manifest -b vic --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/AsTechpro20/local_manifests_blossom.git -b lineage-22 .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune --retry-fetches=5 -j$(nproc --all)
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=Qbhi
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+. build/envsetup.sh
+echo "============="
+
+# Lunch
+lunch orion_blossom-bpa-userdebug
+
+# Build
+make orion -j$(nproc --all)
