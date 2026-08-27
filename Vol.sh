6bc90f0483336d350ecafd6f2ac40724961cbbe3
Alromine95 <abhinavsingh.me5@gmail.com>
2026-08-27 07:50:15 +0530

    Set BUILD_USERNAME and BUILD_HOSTNAME in Vol.sh
    Add environment variables for build username and hostname.

--- Vol.sh
+++ Vol.sh
@@ -15,6 +15,7 @@ git clone https://github.com/Alromine95/Local-manifest.git -b main .repo/local_m
 
 sudo apt-get update && sudo apt-get install patchelf coreutils -y;
 echo 'type mitee_client_device, dev_type;' >> device/xiaomi/blossom/sepolicy/vendor/device.te;
+
 export BUILD_USERNAME=Abhinav
 export BUILD_HOSTNAME=foss
 
