7378752fb40ccaa1d1b166572bd521aba7999812
Alromine95 <abhinavsingh.me5@gmail.com>
2026-08-29 20:25:59 +0530

    Update superior.sh

--- superior.sh
+++ superior.sh
@@ -1,1 +1,38 @@
-superior.sh
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/SuperiorOS/manifest.git -b sixteen-los --depth=1 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/AsTechpro20/local_manifests_blossom.git -b lineage-22 .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Build Sync
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune --retry-fetches=5 -j$(nproc --all)
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Installing packages 
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev liblz4-tool libncurses6 libncurses-dev libsdl1.2-dev libssl-dev libwxgtk3.2-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev -y ;
+sudo apt-get update && sudo apt-get install patchelf coreutils -y
+echo "============="
+echo "packages done"
+echo "============="
+
+# Export
+export BUILD_USERNAME=Qbhi
+export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+echo "======= Export Done ======"
+
+# Set up build environment
+./build-superior.sh blossom -j4
+echo "============="
