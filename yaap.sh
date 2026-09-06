7c68d357cb650fe3de35a58e738cd41c223c9828
Alromine95 <abhinavsingh.me5@gmail.com>
2026-09-06 13:21:28 +0530

    Add installclean command before build
    Added a command to clean up before building.

--- yaap.sh
+++ yaap.sh
@@ -36,6 +36,7 @@ rm -rf build/soong/fsgen;
 sed -i 's/"golang.org\/x\/exp\/maps"/& \n\t"sort"/' build/soong/ui/execution_metrics/execution_metrics.go
 sed -i 's/slices\.Sorted(maps\.Keys(\([^)]*\)))/func() []string { keys := make([]string, 0, len(\1)); for k := range \1 { keys = append(keys, k) }; sort.Strings(keys); return keys }()/' build/soong/ui/execution_metrics/execution_metrics.go
 
+m installclean
 
 echo "build started!..."
 
