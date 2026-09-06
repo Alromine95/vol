e17289343fd9bd04f0705ccbd9648d6d925f5d43
Alromine95 <abhinavsingh.me5@gmail.com>
2026-09-06 18:06:13 +0530

    Update and rename lineage-tornado to lineage-tornado.sh

---
+++ lineage-tornado.sh
@@ -0,0 +1,51 @@
+
+#!/bin/bash
+
+echo "========================"
+echo "removing local manifests"
+echo "========================"
+
+rm -rf .repo/local_manifests;
+rm -rf out/soong/.intermediates/system/sepolicy;
+
+echo "====================="
+echo "      Repo init      "
+echo "====================="
+
+repo init -u https://github.com/LineageOS/android.git -b lineage-24.0 --depth=1 --git-lfs;
+
+git clone https://github.com/Alromine95/local-manifest-tornado.git -b main .repo/local_manifests;
+
+echo "==================="
+echo "     repo sync     "
+echo "==================="
+
+/opt/crave/resync.sh;
+
+sudo apt-get update && sudo apt-get install patchelf coreutils -y;
+
+
+export BUILD_USERNAME=Abhinav
+export BUILD_HOSTNAME=foss
+
+rm -rf build/soong/fsgen;
+
+echo "build started!..."
+
+. build/envsetup.sh;
+lunch lineage_tornado-bp4a-userdebug;
+m bacon 
+
+echo "Upload to GoFile will be started..."
+
+ZIP=$(find out/target/product/tornado -maxdepth 1 -type f -name "*.zip" | head -n 1)
+
+if [ -n "$ZIP" ]; then
+    echo "Uploading $ZIP..."
+    wget https://raw.githubusercontent.com/lordgaruda/GoFile-Upload/refs/heads/master/upload.sh
+    chmod +x upload.sh
+    ./upload.sh "$ZIP"
+else
+    echo "No ROM ZIP found!"
+    exit 1
+fi
