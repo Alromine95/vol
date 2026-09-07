b72489ec56e5d982f42bc17172d22c12935cafc9
Alromine95 <abhinavsingh.me5@gmail.com>
2026-09-07 14:29:53 +0530

    Fix formatting and cleanup commands in yaap.sh

--- yaap.sh
+++ yaap.sh
@@ -5,7 +5,7 @@ echo "removing local manifests"
 echo "========================"
 
 rm -rf .repo/local_manifests;
-rm -rf .repo
+rm -rf .repo;
 rm -rf out/soong/.intermediates/system/sepolicy;
 
 echo "====================="
