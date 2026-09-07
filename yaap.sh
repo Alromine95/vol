3a7d7e496afb935c0a07fddf8270d3b9a84275b6
Alromine95 <abhinavsingh.me5@gmail.com>
2026-09-07 14:29:27 +0530

    Update yaap.sh

--- yaap.sh
+++ yaap.sh
@@ -5,11 +5,8 @@ echo "removing local manifests"
 echo "========================"
 
 rm -rf .repo/local_manifests;
-rm -rf .repo;
+rm -rf .repo
 rm -rf out/soong/.intermediates/system/sepolicy;
-rm -rf vendor/lineage;
-rm -rf build/make;
-rm -rf build/soong;
 
 echo "====================="
 echo "      Repo init      "
