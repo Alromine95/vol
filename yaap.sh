78da95d54cd45671ec5ef2da3bdc667b55cd71a7
Alromine95 <abhinavsingh.me5@gmail.com>
2026-09-13 19:13:10 +0530

    Update yaap.sh

--- yaap.sh
+++ yaap.sh
@@ -144,6 +144,8 @@ sync_repositories() {
     sed -i 's/"golang.org\/x\/exp\/maps"/& \n\t"sort"/' build/soong/ui/execution_metrics/execution_metrics.go
     sed -i 's/slices\.Sorted(maps\.Keys(\([^)]*\)))/func() []string { keys := make([]string, 0, len(\1)); for k := range \1 { keys = append(keys, k) }; sort.Strings(keys); return keys }()/' build/soong/ui/execution_metrics/execution_metrics.go
 
+    echo "==========I AM REMOVING PREBUILTS MAUAHAHAHAHAHAHA============"
+    rm -rf prebuilts/gcc prebuilts/clang
 }
 
 # ==========================================
