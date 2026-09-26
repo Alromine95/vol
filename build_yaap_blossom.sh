7c063527224b23269736b4d7108afd8eb6536318
The Honored One <abhinavsingh.me5@gmail.com>
2026-09-26 17:10:09 +0530

    Add git configuration and update repo commands

--- build_yaap_blossom.sh
+++ build_yaap_blossom.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+git config --global user.name "Abhinav"
+git config --global user.email "abhinav@gmail.com"
 
-repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-14.0 --depth=1 
 
+repo init -u https://github.com/ArrowOS/android_manifest.git -b arrow-14.0 --depth=1 ;
 
+git clone https://github.com/Alromine95/local_manifests_blossom.git -b lineage-21 .repo/local_manifests ;
 
-repo sync -c --no-tags --no-clone-bundle --optimized-fetch -j4 --force-sync
+repo sync -c --no-tags --no-clone-bundle --optimized-fetch -j4 --force-sync ;
 
-. build/envsetup.sh
+. build/envsetup.sh ;
 
-m bacon
+lunch arrow_blossom-userdebug ;
+
+m bacon ;
